23d236762d88016f93103d04d7b23dd0aa1ea760
fernandobf <fernandobravofigueroa@gmail.com>
2025-07-17 14:28:11 +0100

    config deploy

---
+++ src/types/custom.d.ts
@@ -0,0 +1,39 @@
+declare module './components/TextInput' {
+  const TextInput: any;
+  export default TextInput;
+}
+
+declare module './components/FileUploader' {
+  const FileUploader: any;
+  export default FileUploader;
+}
+
+declare module './components/SkillTags' {
+  const SkillTags: any;
+  export default SkillTags;
+}
+
+declare module './components/SkillChart' {
+  const SkillChart: any;
+  export default SkillChart;
+}
+
+declare module './components/ExportPDF' {
+  const ExportPDF: any;
+  export default ExportPDF;
+}
+
+declare module './utils/parseText' {
+  const parseText: any;
+  export default parseText;
+}
+
+declare module './utils/perfilUtils' {
+  const perfilUtils: any;
+  export default perfilUtils;
+}
+
+declare module './utils/textUtils' {
+  const textUtils: any;
+  export default textUtils;
+}
